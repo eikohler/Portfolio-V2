611210e9e89682e9a3fa1c0cbd065d52d84fd812
Eric Kohler <erickohler1600@gmail.com>
2022-07-25 13:55:25 -0400

    updated portfolio with projects images and links

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
 const Modal = ({ onClose, currentPhoto }) => {
-    const {name, description, index} = currentPhoto;
+    const {name, description, link, github, index} = currentPhoto;
     return (
       <div className="modalBackdrop">
         <div className="modalContainer">
           <h3 className="modalTitle">{name}</h3>
-          <img src={require(`../../assets/large/${index}.jpg`)} alt="full preview" />
+          <img src={require(`../../assets/portfolio-images/${index}.jpg`)} alt="full preview" />
           <p>{description}</p>
+          <div className='mb-4 links'>
+            <a href={link} target="_blank" rel="noopener noreferrer"><i className="fa fa-television"></i></a>
+            <a href={github} target="_blank" rel="noopener noreferrer"><i class="fa fa-github"></i></a>
+          </div>
           <button onClick={onClose} type="button">Close this modal</button>
         </div>
     </div>
